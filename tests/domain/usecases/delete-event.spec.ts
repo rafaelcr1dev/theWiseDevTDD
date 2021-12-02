d781cf2734836abb5cd8bab9f999bbb05f3ce11d
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-12-02 19:30:37 -0300

    move SUT creation to a factory method

--- tests/domain/usecases/delete-event.spec.ts
+++ tests/domain/usecases/delete-event.spec.ts
@@ -1,20 +1,49 @@
 class DeleteEvent {
   // loadGroupRepository: LoadGroupRepository
   constructor (
-    private readonly loadGroupRepository: LoadGroupRepository
+    private readonly loadGroupRepository: ILoadGroupRepository
   ) {}
 
-  async perform (input: { id: string, userId: string}): Promise<void> {
-
+  async perform ({ id }: { id: string, userId: string}): Promise<void> {
+    await this.loadGroupRepository.load({ eventId: id })
   }
 }
 
-class LoadGroupRepository {
-  eventId: string | undefined
+interface ILoadGroupRepository {
+  load: (input: {
+    eventId: string
+  }) => Promise<void>
+}
+/*
+  Mock = Propriedades usadas para observar coisas (preucupacao com as entradas)
+  Stub = Retorno fixo de uma funcao
+*/
+class LoadGroupRepositoryMock implements ILoadGroupRepository {
+  eventId?: string
+  callsCount = 0
+  // eventId: string | undefined
 
-//   constructor (id: string) {
-//     this.eventId = id
-//   }
+  //   constructor (id: string) {
+  //     this.eventId = id
+  //
+  async load ({ eventId }: { eventId: string }): Promise<void> {
+    this.eventId = eventId
+    this.callsCount++
+  }
+}
+
+type SutTypes = {
+  sut: DeleteEvent
+  loadGroupRepository: LoadGroupRepositoryMock
+}
+
+const makeSut = (): SutTypes => {
+  const loadGroupRepository = new LoadGroupRepositoryMock()
+  const sut = new DeleteEvent(loadGroupRepository)
+  return {
+    sut,
+    loadGroupRepository
+  }
 }
 
 describe('DeleteEvent', () => {
@@ -23,8 +52,7 @@ describe('DeleteEvent', () => {
     // 3A - Arrange, Act, Assert (Organizar, agir, afirmar)
     const id = 'any_event_id'
     const userId = 'any_user_id'
-    const loadGroupRepository = new LoadGroupRepository()
-    const sut = new DeleteEvent(loadGroupRepository)
+    const { sut, loadGroupRepository } = makeSut()
 
     await sut.perform({
       id,
@@ -32,5 +60,6 @@ describe('DeleteEvent', () => {
     })
 
     expect(loadGroupRepository.eventId).toBe(id)
+    expect(loadGroupRepository.callsCount).toBe(1)
   })
 })
